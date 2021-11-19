67b1b81c09d5da0d4ed589862f7e57fde21d6f70
raihan kawser <raihankawser29@gmail.com>
2021-11-19 19:57:31 +0600

    new items

--- index.js
+++ index.js
@@ -18,8 +18,40 @@ async function run(){
         await client.connect();
        const database = client.db('bicycle_project');
        const bicycleConnection = database.collection('bicycle');
+       const userConnection = database.collection('userOrders');
+       const reviewConnection = database.collection('userReview');
 
 
+       //post user review
+       app.post('/userReview',async(req,res)=>{
+        const data = req.body;
+        const result = await reviewConnection.insertOne(data);
+        res.json(result)
+       })
+       
+       //post user orders
+       app.post('/bicycle',async(req,res)=>{
+        const data = req.body;
+        const result = await bicycleConnection.insertOne(data);
+        res.json(result)
+       })
+
+
+       //post user orders
+       app.post('/userOrders',async(req,res)=>{
+        const data = req.body;
+        const result = await userConnection.insertOne(data);
+        res.json(result)
+       })
+
+        //api load review
+        app.get('/userReview',async(req,res)=>{
+            const cursor = reviewConnection.find({})
+            const result = await cursor.toArray();
+            res.send(result)
+           })
+
+       
        //api load
        app.get('/bicycle',async(req,res)=>{
         const cursor = bicycleConnection.find({}).limit(6)
@@ -27,6 +59,36 @@ async function run(){
         res.send(result)
        })
 
+       //api load explore
+       app.get('/bicycles',async(req,res)=>{
+        const cursor = bicycleConnection.find({})
+        const result = await cursor.toArray();
+        res.send(result)
+       })
+
+       //load single service
+       app.get('/bicycle/:id',async(req,res)=>{
+           const id = req.params.id;
+           const qeury = {_id:ObjectId(id)};
+           const result = await bicycleConnection.findOne(qeury);
+           res.json(result);
+       })
+
+
+       //api load
+       app.get('/userOrders',async(req,res)=>{             
+        const cursor = userConnection.find({})
+        const result = await cursor.toArray();
+        res.send(result)
+      })
+
+      app.get('/userOrders/:email',async(req,res)=>{
+          const email= req.params.email;
+           const  qeury ={userEmail: email}         
+        const cursor = userConnection.find(qeury)
+        const result = await cursor.toArray();
+        res.send(result)
+      })
 
     }
   
