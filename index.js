0ad304934b8f06e8610adfb7fb6f3b24b9f05612
raihan kawser <raihankawser29@gmail.com>
2021-11-19 22:47:08 +0600

    dsuifhudsi

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 // middlewire
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname + '/public'))
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hahmn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
